e38a4623d684db9e8eef9dd6ab34758453c4b090
Sreeram Unny <unny.sreeram@gmail.com>
2025-11-30 00:02:56 +0100

    Refactored case from smaller to upper.

--- int_float.py
+++ int_float.py
@@ -2,18 +2,18 @@
 
 # Integers
 # An integer (int) is a whole number without a decimal point.
-whole_number = 10
-print("Integer value:", whole_number)
-print("Type of whole_number:", type(whole_number))
+WHOLE_NUMBER = 10
+print("Integer value:", WHOLE_NUMBER)
+print("Type of whole_number:", type(WHOLE_NUMBER))
 
 # Floating Point Numbers
 # A float is a number that has a decimal point.    
-decimal_number = 10.5
-print("Float value:", decimal_number)
-print("Type of decimal_number:", type(decimal_number))
+DECIMAL_NUMBER = 10.5
+print("Float value:", DECIMAL_NUMBER)
+print("Type of decimal_number:", type(DECIMAL_NUMBER))
 
 # Boolean Values
 # A Boolean (bool) represents one of two values: True or False.
-boolean_value = True
-print("Boolean value:", boolean_value)
-print("Type of boolean_value:", type(boolean_value))    
+BOOLEAN_VALUE = True
+print("Boolean value:", BOOLEAN_VALUE)
+print("Type of boolean_value:", type(BOOLEAN_VALUE))    
