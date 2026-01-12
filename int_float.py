ccb30603eb88c1693a4ff6650be2bb18c0cfa25e
Sreeram Unny <unny.sreeram@gmail.com>
2026-01-13 00:19:26 +0100

    Added docstrings at the top of the code.

--- int_float.py
+++ int_float.py
@@ -1,18 +1,19 @@
-# This code snipet helps explain the concept of int and float data types in Python.
+""" This code snipet helps explain the concept of various data types in Python """
 
 # Integers
 # An integer (int) is a whole number without a decimal point.
-whole_number = 10
-print("Integer value:", whole_number)
-print("Type of whole_number:", type(whole_number))
+WHOLE_NUMBER = 10
+print("Integer value:", WHOLE_NUMBER)
+print("Type of whole_number:", type(WHOLE_NUMBER))
 
 # Floating Point Numbers
 # A float is a number that has a decimal point.    
 DECIMAL_NUMBER = 10.5
 print("Float value:", DECIMAL_NUMBER)
 print("Type of decimal_number:", type(DECIMAL_NUMBER))
+
 # Boolean Values
 # A Boolean (bool) represents one of two values: True or False.
-boolean_value = True
-print("Boolean value:", boolean_value)
-print("Type of boolean_value:", type(boolean_value))    
+BOOLEAN_VALUE = True
+print("Boolean value:", BOOLEAN_VALUE)
+print("Type of boolean_value:", type(BOOLEAN_VALUE))
