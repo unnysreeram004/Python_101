e7ee7851ca66bc683bba971678bd6f9bd8a30c9c
Sreeram Unny <unny.sreeram@gmail.com>
2026-01-13 00:06:20 +0100

    Changed case from LOWER to UPPER

--- int_float.py
+++ int_float.py
@@ -1,11 +1,11 @@
 # This code snipet helps explain the concept of int and float data types in Python.
 
 # An integer (int) is a whole number without a decimal point.
-whole_number = 10
-print("Integer value:", whole_number)
-print("Type of whole_number:", type(whole_number))
+WHOLE_NUMBER = 10
+print("Integer value:", WHOLE_NUMBER)
+print("Type of whole_number:", type(WHOLE_NUMBER))
 
 # A float is a number that has a decimal point.    
-decimal_number = 10.5
-print("Float value:", decimal_number)
-print("Type of decimal_number:", type(decimal_number))
+DECIMAL_NUMBER = 10.5
+print("Float value:", DECIMAL_NUMBER)
+print("Type of decimal_number:", type(DECIMAL_NUMBER))
